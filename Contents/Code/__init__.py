a100178ff2342532f713734b61483aa4c50f0420
Oncleben31 <oncleben31@gmail.com>
2010-09-07 22:34:33 +0200

    Rewrite the code to use a python dictionary for languages.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -38,6 +38,9 @@ EXTRACT_AS_KEYWORDS = ['uk','us','bbc']
 
 netLock = Thread.Lock()
 
+# Language table
+THETVDB_LANGUAGES_CODE = {'zh': '6', 'en':'7', 'sv': '8', 'no': '9', 'da': '10', 'fi': '11', 'nl': '13', 'de': '14', 'it': '15', 'es': '16', 'fr': '17', 'pl': '18', 'hu': '19', 'el': '20', 'tr': '21', 'ru': '22', 'he': '24', 'ja': '25', 'pt': '26'}
+
 # Keep track of success/failures in a row.
 successCount = 0
 failureCount = 0
